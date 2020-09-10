037ca25237eea0540e3847d21569ed908199c352
sqlchan <1062511908@qq.com>
2020-09-10 22:43:07 +0800

    top100  _90

---
+++ src/main/java/N70_89/LargestRectangleArea_84.java
@@ -0,0 +1,43 @@
+package N70_89;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 84. 柱状图中最大的矩形
+ * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+ *
+ * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+ *
+ * 我们可以遍历每根柱子，以当前柱子 i 的高度作为矩形的高，那么矩形的宽度边界即为向左找到第一个高度小于当前柱体 i 的柱体，向右找到第一个高度小于当前柱体 i 的柱体。
+ *
+ * 对于每个柱子我们都如上计算一遍以当前柱子作为高的矩形面积，最终比较出最大的矩形面积即可。
+ *
+ * 我们每遍历到当前柱体 i 时：
+ *
+ * 上述写法中，我们需要再嵌套一层 while 循环来向左找到第一个比柱体 i 高度小的柱体，这个过程是 O(N) 的；
+ * 那么我们可以 O(1) 的获取柱体 i 左边第一个比它小的柱体吗？答案就是单调增栈，因为对于栈中的柱体来说，栈中下一个柱体就是左边第一个高度小于自身的柱体。
+ * 因此做法就很简单了，我们遍历每个柱体，若当前的柱体高度大于等于栈顶柱体的高度，就直接将当前柱体入栈，否则若当前的柱体高度小于栈顶柱体的高度，说明当前栈顶柱体找到了右边的第一个小于自身的柱体，那么就可以将栈顶柱体出栈来计算以其为高的矩形的面积了。
+ */
+public class LargestRectangleArea_84 {
+    public int largestRectangleArea(int[] heights) {
+        // 这里为了代码简便，在柱体数组的头和尾加了两个高度为 0 的柱体。
+        int[] tmp = new int[heights.length + 2];
+        System.arraycopy(heights, 0, tmp, 1, heights.length);
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        int area = 0;
+        for (int i = 0; i < tmp.length; i++) {
+            // 对栈中柱体来说，栈中的下一个柱体就是其「左边第一个小于自身的柱体」；
+            // 若当前柱体 i 的高度小于栈顶柱体的高度，说明 i 是栈顶柱体的「右边第一个小于栈顶柱体的柱体」。
+            // 因此以栈顶柱体为高的矩形的左右宽度边界就确定了，可以计算面积🌶️ ～
+            while (!stack.isEmpty() && tmp[i] < tmp[stack.peek()]) {
+                int h = tmp[stack.pop()];
+                area = Math.max(area, (i - stack.peek() - 1) * h);
+            }
+            stack.push(i);
+        }
+
+        return area;
+    }
+}
--- src/main/java/N70_89/MinWindow_76.java
+++ src/main/java/N70_89/MinWindow_76.java
@@ -1,5 +1,9 @@
 package N70_89;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * 76. 最小覆盖子串
  * 给你一个字符串 S、一个字符串 T 。请你设计一种算法，可以在 O(n) 的时间复杂度内，从字符串 S 里面找出：包含 T 所有字符的最小子串。
@@ -9,7 +13,60 @@ package N70_89;
  * 输入：S = "ADOBECODEBANC", T = "ABC"
  * 输出："BANC"
  *
- * 
+ * 我们可以用滑动窗口的思想解决这个问题，在滑动窗口类型的问题中都会有两个指针。一个用于「延伸」现有窗口的 rr 指针，和一个用于「收缩」窗口的 ll 指针。在任意时刻，只有一个指针运动，而另一个保持静止。我们在 ss 上滑动窗口，通过移动 rr 指针不断扩张窗口。当窗口包含 tt 全部所需的字符后，如果能收缩，我们就收缩窗口直到得到最小窗口。
+ *
+ *
+ * 如何判断当前的窗口包含所有 tt 所需的字符呢？我们可以用一个哈希表表示 tt 中所有的字符以及它们的个数，用一个哈希表动态维护窗口中所有的字符以及它们的个数，如果这个动态表中包含 tt 的哈希表中的所有字符，并且对应的个数都不小于 tt 的哈希表中各个字符的个数，那么当前的窗口是「可行」的。
+ *
+ * 注意：这里 tt 中可能出现重复的字符，所以我们要记录字符的个数。
+ *
  */
 public class MinWindow_76 {
+    class Solution {
+        Map<Character, Integer> ori = new HashMap<Character, Integer>();
+        Map<Character, Integer> cnt = new HashMap<Character, Integer>();
+
+        public String minWindow(String s, String t) {
+            int tLen = t.length();
+            for (int i = 0; i < tLen; i++) {
+                char c = t.charAt(i);
+                ori.put(c, ori.getOrDefault(c, 0) + 1);
+            }
+            int l = 0, r = -1;
+            int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+            int sLen = s.length();
+            while (r < sLen) {
+                ++r;
+                if (r < sLen && ori.containsKey(s.charAt(r))) {
+                    cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+                }
+                while (check() && l <= r) {
+                    if (r - l + 1 < len) {
+                        len = r - l + 1;
+                        ansL = l;
+                        ansR = l + len;
+                    }
+                    if (ori.containsKey(s.charAt(l))) {
+                        cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                    }
+                    ++l;
+                }
+            }
+            return ansL == -1 ? "" : s.substring(ansL, ansR);
+        }
+
+        public boolean check() {
+            Iterator iter = ori.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry entry = (Map.Entry) iter.next();
+                Character key = (Character) entry.getKey();
+                Integer val = (Integer) entry.getValue();
+                if (cnt.getOrDefault(key, 0) < val) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
 }
